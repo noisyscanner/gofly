bfb58481647139b00a72a7462c4bfebb46d3f3d2
Brad Reed 😎 <brad@bradreed.co.uk>
2020-06-28 21:38:11 +0100

    Allow to supply MIGRATIONS_PATH as env var

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,15 +1,26 @@
 package migrate
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/golang-migrate/migrate"
 	_ "github.com/golang-migrate/migrate/database/mysql"
 	_ "github.com/golang-migrate/migrate/source/file"
 	"github.com/noisyscanner/gofly/gofly"
 )
 
+func getMigrationsPath() string {
+	envVar := os.Getenv("MIGRATIONS_PATH")
+	if envVar != "" {
+		return envVar
+	}
+	return "/migrations"
+}
+
 func getMigrate(configService gofly.ConfigService) (*migrate.Migrate, error) {
 	config := configService.GetConfig()
-	return migrate.New("file:///migrations/", config.DBString())
+	return migrate.New(fmt.Sprintf("file://%s", getMigrationsPath()), config.DBString())
 }
 
 func Up(configService gofly.ConfigService) error {
